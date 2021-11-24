1e08e11a9a05f5f2f47fe1a6445fe5725b4e1305
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2021-11-25 08:59:12 +0900

    mlp mixer 코드 업데이트

---
+++ torch/model/mlpMixer.py
@@ -0,0 +1,5 @@
+import torch
+import numpy as np
+import torch.nn as nn
+from einops.layers.torch import Rearrange, Reduce
+
---
+++ torch/torch_mlpMixer_train.py
@@ -0,0 +1,35 @@
+if __name__ == "__main__":
+    from torchsummary import summary
+    import pdb
+    import torch
+    from util.helper import MLPMixer
+
+    # --------- base_model_param ---------
+    in_channels = 3
+    hidden_size = 512
+    num_classes = 1000
+    patch_size = 16
+    resolution = 224
+    number_of_layers = 8
+    token_dim = 256
+    channel_dim = 2048
+    # ------------------------------------
+
+    model = MLPMixer(
+        in_channels=in_channels,
+        dim=hidden_size,
+        num_classes=num_classes,
+        patch_size=patch_size,
+        image_size=resolution,
+        depth=number_of_layers,
+        token_dim=token_dim,
+        channel_dim=channel_dim
+    )
+    img = torch.rand(2, 3, 224, 224)
+    output = model(img)
+    print(output.shape)
+
+    # summary(model, input_size=(3, 224, 224), device='cpu')
+    parameters = filter(lambda p: p.requires_grad, model.parameters())
+    parameters = sum([np.prod(p.size()) for p in parameters]) / 1_000_000  # 18.528 million
+    print('Trainable Parameters: %.3fM' % parameters)
