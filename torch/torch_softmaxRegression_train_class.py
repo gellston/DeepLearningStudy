627b008129f0024db241aa06cd9d194dce86f335
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2021-11-26 18:28:04 +0900

    softmaxRegression_train_mnist업데이트

--- torch/torch_softmaxRegression_train_class.py
+++ torch/torch_softmaxRegression_train_class.py
@@ -3,9 +3,7 @@ import torch.nn.functional as F
 import torch.nn as nn
 import torch.optim as optim
 
-
 from model.SoftmaxClassifierModel import SoftmaxClassifierModel
-
 
 x_train = [[1, 2, 1, 1],
            [2, 1, 3, 2],
@@ -20,17 +18,13 @@ y_train = [2, 2, 2, 1, 1, 1, 0, 0]
 
 x_train = torch.FloatTensor(x_train)
 y_train = torch.LongTensor(y_train)
-
 print(x_train.shape)
 print(y_train.shape)
 
 y_one_hot = torch.zeros(8, 3)#heightxwidth
 y_one_hot.scatter_(1, y_train.unsqueeze(1), 1)
-
-
 print(y_one_hot.shape)
 print(y_one_hot)
-
 
 
 # 모델 초기화
@@ -39,7 +33,6 @@ model = SoftmaxClassifierModel()
 # optimizer 설정
 optimizer = optim.SGD(model.parameters(), lr=0.1)
 
-
 nb_epochs = 2000
 for epoch in range(nb_epochs + 1):
 
@@ -47,11 +40,7 @@ for epoch in range(nb_epochs + 1):
     prediction = model(x_train)
 
     # 비용 함수
-    hypothesis = F.softmax(prediction, dim=1)
-    cost = (y_one_hot * -torch.log(hypothesis)).sum(dim=1).mean()
-
-    F.cross_entropy()
-
+    cost = F.cross_entropy(prediction, y_train)
 
     # cost로 H(x) 개선
     optimizer.zero_grad()
@@ -61,7 +50,6 @@ for epoch in range(nb_epochs + 1):
     # 100번마다 로그 출력
     if epoch % 100 == 0:
         print('Epoch {:4d}/{} Cost: {:.6f}'.format(epoch, nb_epochs, cost.item()))
-
 
 
 print('==== result ====')
