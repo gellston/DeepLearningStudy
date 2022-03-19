efa562af948ba871ba1da09797a99d0337ad454d
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2022-03-19 21:17:40 +0900

    update PerceptronSimpleMnist

--- torch/model/PerceptronSimpleMnist.py
+++ torch/model/PerceptronSimpleMnist.py
@@ -1,14 +1,19 @@
 #import numpy as np
 import torch
 import torch.nn as nn
-#import torch.nn.functional as F
+import torch.nn.functional as F
 
 class PerceptronSimpleMnist(nn.Module):
 
     def __init__(self):
         super(PerceptronSimpleMnist, self).__init__()
         self.hidden1 = nn.Linear(784, 128) #--> 784 *100 = 78400 W , 28x28
+        self.drop_out1 = nn.Dropout(p=0.3)
+
         self.hidden2 = nn.Linear(128, 64)
+        self.drop_out2 = nn.Dropout(p=0.3)
+
+
         self.hidden3 = nn.Linear(64, 10)
 
         self.softmax = nn.Softmax()
@@ -19,7 +24,11 @@ class PerceptronSimpleMnist(nn.Module):
 
     def forward(self, x):
         x = self.hidden1(x)
+        x = F.relu(x)
+        x = self.drop_out1(x)
         x = self.hidden2(x)
+        x = F.relu(x)
+        x = self.drop_out2(x)
         x = self.hidden3(x)
         x = self.softmax(x) ## 데이터셋의 이미지상에 객체가 1개가 나오는걸 보장
 
