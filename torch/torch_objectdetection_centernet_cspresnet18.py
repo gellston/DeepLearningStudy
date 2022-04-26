centernet prediction bbox calculation bug fix

torch/torch_objectdetection_centernet_cspresnet18.py
@@ -11,7 +11,6 @@ from torch.optim.lr_scheduler import CyclicLR
 from util.centernet_helper import batch_loader
 from util.centernet_helper import batch_accuracy
 from util.losses import CenterNetLoss
-from util.scheduler import WarmupConstantSchedule
 
 from model.CSPResnet18 import CSPResnet18
 from model.CSPResnet18CenterNet import CSPResnet18CenterNet
@@ -33,8 +32,10 @@ if device == 'cuda':
 training_epochs = 300
 batch_size = 22
 target_accuracy = 0.90
-learning_rate = 0.00025
+learning_rate = 0.00005
 accuracy_threshold = 0.5
+class_score_threshold = 0.3,
+iou_threshold = 0.5,
 input_image_width = 512
 input_image_height = 512
 feature_map_scale_factor = 4
@@ -70,18 +71,14 @@ object_detection_transform = torchvision.transforms.Compose([
         #torchvision.transforms.Grayscale(num_output_channels=3),
         torchvision.transforms.ToTensor()
     ])
-
 objectDetectionDataset = torchvision.datasets.WIDERFace(root="C://Github//Dataset//",
                                                         split="train",
                                                         transform=object_detection_transform,
                                                         download=False)
-
-
 object_detection_data_loader = DataLoader(dataset=objectDetectionDataset,
                                           batch_size=1,  # 배치 크기는 100
                                           shuffle=True,
                                           drop_last=True)
-
 total_batch = int(len(object_detection_data_loader) / batch_size)
 print('total batch=', total_batch)
 # object detection dataset loader
@@ -90,18 +87,16 @@ print('total batch=', total_batch)
 
 CSPResnet18CenterNet.train()
 criterion = CenterNetLoss(alpha=1, gamma=1, beta=0.1)
-#optimizer = torch.optim.SGD(CSPResnet18CenterNet.parameters(), lr=learning_rate, weight_decay=1e-6, momentum=0.9)
-optimizer = torch.optim.Adam(CSPResnet18CenterNet.parameters(), lr=learning_rate, weight_decay=1e-6)
-#scheduler = CyclicLR(optimizer, base_lr=learning_rate, max_lr=0.05, step_size_up=30, mode='triangular2')
-#scheduler = WarmupConstantSchedule(optimizer, warmup_steps=3)
+optimizer = torch.optim.SGD(CSPResnet18CenterNet.parameters(), lr=learning_rate, weight_decay=1e-6, momentum=0.9)
+scheduler = CyclicLR(optimizer, base_lr=learning_rate, max_lr=0.05, step_size_up=10, mode='triangular2')
 
-
-print("object detection training start")
 for epoch in range(training_epochs): # 앞서 training_epochs의 값은 15로 지정함.
     avg_cost = 0
     avg_acc = 0
+
+
+    print('current learning rate =',  scheduler.get_last_lr())
     for batch_index in range(total_batch):
-        #print('batch index = ', batch_index)
         batch = batch_loader(object_detection_data_loader,
                              batch_size,
                              input_image_width,
@@ -141,12 +136,13 @@ for epoch in range(training_epochs): # 앞서 training_epochs의 값은 15로 �
         avg_cost += (cost / total_batch)
         optimizer.step()
 
+
         """
         validation = batch_accuracy(input_image_width=input_image_width,
                                     input_image_height=input_image_height,
                                     scale_factor=feature_map_scale_factor,
-                                    score_threshold=0.3,
-                                    iou_threshold=0.5,
+                                    score_threshold=class_score_threshold,
+                                    iou_threshold=iou_threshold,
                                     gaussian_map_batch=prediction_heatmap,
                                     size_map_batch=prediction_sizemap,
                                     offset_map_batch=prediction_offsetmap,
@@ -162,13 +158,13 @@ for epoch in range(training_epochs): # 앞서 training_epochs의 값은 15로 �
         cv2.namedWindow("heatmap", cv2.WINDOW_NORMAL)
         cv2.resizeWindow('heatmap', input_image_width, input_image_height)
         cv2.imshow('heatmap', heatmap_image)
-
+        cv2.waitKey(10)
 
         heatmap_label = label_heatmap[0].detach().permute(1, 2, 0).squeeze(0).cpu().numpy().astype(np.float32)
         cv2.namedWindow("heatmap_label", cv2.WINDOW_NORMAL)
         cv2.resizeWindow('heatmap_label', input_image_width, input_image_height)
         cv2.imshow('heatmap_label', heatmap_label)
-
+        cv2.waitKey(10)
 
 
         input_image = label_image[0].detach().permute(1, 2, 0).cpu().numpy()
@@ -203,8 +199,7 @@ for epoch in range(training_epochs): # 앞서 training_epochs의 값은 15로 �
         accuracy = correct_prediction.float().mean()
         avg_acc += (accuracy / total_batch)
         """
-
-    #scheduler.step()
+    scheduler.step()
 
     print("학습중간에 저장")
     ## no Train Model Save
@@ -224,7 +219,6 @@ for epoch in range(training_epochs): # 앞서 training_epochs의 값은 15로 �
         break;
 
 
-print("object detection training end")
 ## no Train Model Save
 CSPResnet18CenterNet.eval()
 CSPResnet18.eval()
@@ -233,4 +227,5 @@ torch.jit.save(compiled_model_backbone, "C://Github//DeepLearningStudy//trained_
 compiled_model = torch.jit.script(CSPResnet18CenterNet)
 torch.jit.save(compiled_model, "C://Github//DeepLearningStudy//trained_model//TRAIN_WIDERFACE(CSPResnet18CenterNet).pt")
 ## no Train Model Save
+
 print('Learning finished')

torch/torch_perceptron_mnist_simple_train.py
@@ -26,7 +26,7 @@ print("cudnn version:{}".format(torch.backends.cudnn.version()))
 training_epochs = 100
 batch_size = 100
 learning_rate = 0.0001
-target_accuracy = 0.98
+target_accuracy = 0.90
 
 
 # MNIST dataset

torch/util/centernet_helper.py
@@ -128,21 +128,10 @@ def batch_loader(loader, batch_size, input_width, input_height, feature_map_scal
             bbox_list.append((input_image_box_x, input_image_box_y, input_image_box_width, input_image_box_height))
 
             ##Clamping x,y
-            clamp_feature_box_x = np.clip(feature_box_x, 0, feature_map_width)
-            clamp_feature_box_y = np.clip(feature_box_y, 0, feature_map_height)
+            clamp_feature_box_x = np.clip(feature_box_x, 0, feature_map_width-1)
+            clamp_feature_box_y = np.clip(feature_box_y, 0, feature_map_height-1)
             clamp_feature_box_x = int(clamp_feature_box_x)
             clamp_feature_box_y = int(clamp_feature_box_y)
-
-
-            assign_area_min_x = (int)(clamp_feature_box_x) - 1
-            assign_area_max_x = (int)(clamp_feature_box_x) + 1
-            assign_area_min_y = (int)(clamp_feature_box_y) - 1
-            assign_area_max_y = (int)(clamp_feature_box_y) + 1
-
-            assign_area_min_x = (int)(np.clip(assign_area_min_x, 0, feature_map_width))
-            assign_area_max_x = (int)(np.clip(assign_area_max_x, 0, feature_map_width))
-            assign_area_min_y = (int)(np.clip(assign_area_min_y, 0, feature_map_height))
-            assign_area_max_y = (int)(np.clip(assign_area_max_y, 0, feature_map_height))
 
 
             ##Calculating offset x, y
@@ -150,12 +139,12 @@ def batch_loader(loader, batch_size, input_width, input_height, feature_map_scal
             feature_box_offset_y = feature_box_y - clamp_feature_box_y
 
             ##Fill Size Map
-            size_map[0][0][assign_area_min_y:assign_area_max_y][assign_area_min_x:assign_area_max_x] = feature_box_width
-            size_map[0][1][assign_area_min_y:assign_area_max_y][assign_area_min_x:assign_area_max_x] = feature_box_height
+            size_map[0][0][clamp_feature_box_y][clamp_feature_box_x] = feature_box_width
+            size_map[0][1][clamp_feature_box_y][clamp_feature_box_x] = feature_box_height
 
             ##Fill Offset Map
-            offset_map[0][0][assign_area_min_y:assign_area_max_y][assign_area_min_x:assign_area_max_x] = feature_box_offset_x
-            offset_map[0][1][assign_area_min_y:assign_area_max_y][assign_area_min_x:assign_area_max_x] = feature_box_offset_y
+            offset_map[0][0][clamp_feature_box_y][clamp_feature_box_x] = feature_box_offset_x
+            offset_map[0][1][clamp_feature_box_y][clamp_feature_box_x] = feature_box_offset_y
 
             ##Gaussian Map
             generate_heatmap(gaussian_map[:, :, 0], clamp_feature_box_x, clamp_feature_box_y, feature_box_height, feature_box_width)
@@ -210,10 +199,11 @@ def batch_accuracy(input_image_width,
                 if gaussian_map[0, feature_y, feature_x].item() > score_threshold:
                     offset_x = offset_map[0, feature_y, feature_x].item()
                     offset_y = offset_map[1, feature_y, feature_x].item()
-                    final_box_x = (feature_x + offset_x) / feature_image_width * input_image_width
-                    final_box_y = (feature_y + offset_y) / feature_image_height * input_image_height
                     final_box_width = size_map[0, feature_y, feature_x].item() / feature_image_width * input_image_width
                     final_box_height = size_map[1, feature_y, feature_x].item() / feature_image_height * input_image_height
+                    final_box_x = (feature_x + offset_x) / feature_image_width * input_image_width - final_box_width / 2
+                    final_box_y = (feature_y + offset_y) / feature_image_height * input_image_height - final_box_height / 2
+
                     probability = gaussian_map[0, feature_y, feature_x].item()
                     if final_box_width != 0 and final_box_height != 0:
                         prediction_box_list.append((probability, final_box_x, final_box_y, final_box_width, final_box_height))

torch/util/losses.py
@@ -267,11 +267,7 @@ class CenterNetLoss(torch.nn.Module):
         self.alpha = alpha
         self.gamma = gamma
         self.beta = beta
-
         self.focal_loss = modified_focal_loss
-        self.size_loss = reg_l1_loss
-        self.offset_loss = reg_l1_loss
-
 
     def forward(self, prediction_features,  ##sigmoid focal loss함수에서 sigmoid를 쒸우기때문에 sigmoid없는 feature map 입력
                       prediction_sizemap,
@@ -280,9 +276,15 @@ class CenterNetLoss(torch.nn.Module):
                       label_sizemap,
                       label_offsetmap):
 
-
+        size_num = torch.sum((label_sizemap > 0)).item()
         sum_class_loss = self.focal_loss(torch.sigmoid(prediction_features), label_heatmap) * self.alpha
-        sum_size_loss = self.size_loss(prediction_sizemap, label_sizemap) * self.beta
-        sum_offset_loss = self.offset_loss(prediction_offsetmap, label_offsetmap) * self.gamma
+        sum_size_loss = F.smooth_l1_loss(prediction_sizemap,
+                                         label_sizemap,
+                                         reduction='sum',
+                                         beta=1.0) / size_num * self.beta
+        sum_offset_loss = F.smooth_l1_loss(prediction_offsetmap,
+                                           label_offsetmap,
+                                           reduction='sum',
+                                           beta=1.0) / size_num * self.gamma
 
         return sum_class_loss + sum_size_loss + sum_offset_loss