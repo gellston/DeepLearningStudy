41e182bc062526b1b189e91e23bb34b97b3a64e6
BongHoe Koo <42884297+gellston@users.noreply.github.com>
2022-06-17 20:06:31 +0900

    NFResnet update

---
+++ torch/model/NFResNet18.py
@@ -0,0 +1,102 @@
+import torch
+import torch.nn.functional as F
+from util.helper import WSConv2d
+from util.helper import NFResidualBlock
+
+
+class NFResNet18(torch.nn.Module):
+
+    def __init__(self, class_num=5):
+        super(NFResNet18, self).__init__()
+
+        self.class_num = class_num
+
+        self.layer1 = torch.nn.Sequential(
+            WSConv2d(in_channels=3,
+                     out_channels=64,
+                     stride=2,
+                     padding=1,
+                     kernel_size=7),
+            torch.nn.MaxPool2d(kernel_size=3,
+                               padding=1,
+                               stride=2)
+        )
+
+        self.layer2 = torch.nn.Sequential(
+            NFResidualBlock(in_dim=64,
+                            mid_dim=64,
+                            out_dim=64,
+                            stride=1),
+            NFResidualBlock(in_dim=64,
+                            mid_dim=64,
+                            out_dim=64,
+                            stride=1),
+        )
+
+
+        self.layer3 = torch.nn.Sequential(
+            NFResidualBlock(in_dim=64,
+                            mid_dim=128,
+                            out_dim=128,
+                            stride=2),
+            NFResidualBlock(in_dim=128,
+                            mid_dim=128,
+                            out_dim=128,
+                            stride=1),
+        )
+
+        self.layer4 = torch.nn.Sequential(
+            NFResidualBlock(in_dim=128,
+                            mid_dim=256,
+                            out_dim=256,
+                            stride=2),
+            NFResidualBlock(in_dim=256,
+                            mid_dim=256,
+                            out_dim=256,
+                            stride=1),
+        )
+        self.layer5 = torch.nn.Sequential(
+            NFResidualBlock(in_dim=256,
+                            mid_dim=512,
+                            out_dim=512,
+                            stride=2),
+            NFResidualBlock(in_dim=512,
+                            mid_dim=512,
+                            out_dim=512,
+                            stride=1),
+        )
+        self.gap = torch.nn.AdaptiveAvgPool2d(1)
+        self.conv_class = torch.nn.Conv2d(kernel_size=1,
+                                          in_channels=512,
+                                          out_channels=class_num)
+
+        for m in self.modules():
+            if isinstance(m, torch.nn.Conv2d):
+                torch.nn.init.kaiming_normal_(m.weight, mode='fan_in', nonlinearity='linear')
+
+            # Zero-initialize the last BN in each residual branch,
+            # so that the residual branch starts with zeros, and each residual block behaves like an identity.
+            # This improves the model by 0.2~0.3% according to https://arxiv.org/abs/1706.02677
+        """ 공부 필요
+        if zero_init_residual:
+            for m in self.modules():
+                if isinstance(m, Bottleneck):
+                    # type: ignore[arg-type]
+                    nn.init.constant_(m.bn3.weight, 0)
+                elif isinstance(m, BasicBlock):
+                    # type: ignore[arg-type]
+                    nn.init.constant_(m.bn2.weight, 0)
+            공부 필요
+        """
+
+    def forward(self, x):
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+        x = self.layer5(x)
+        x = self.gap(x)
+        x = self.conv_class(x)
+        x = x.view([-1, self.class_num])
+        x = torch.softmax(x, dim=1)
+        return x
---
+++ torch/torch_fiat_classification_nfresnet18.py
@@ -0,0 +1,106 @@
+import torch
+import torch.nn as nn
+import random
+
+
+from ptflops import get_model_complexity_info
+from torchsummary import summary
+from torch.utils.data import DataLoader
+
+from model.NFResNet18 import NFResNet18
+from util.FIATClassificationDataset import FIATClassificationDataset
+from util.nf_helper import AGC
+
+
+USE_CUDA = torch.cuda.is_available() # GPU를 사용가능하면 True, 아니라면 False를 리턴
+device = torch.device("cuda" if USE_CUDA else "cpu") # GPU 사용 가능하면 사용하고 아니면 CPU 사용
+print("다음 기기로 학습합니다:", device)
+
+
+# for reproducibility
+random.seed(777)
+torch.manual_seed(777)
+if device == 'cuda':
+    torch.cuda.manual_seed_all(777)
+
+
+## Hyper parameter
+training_epochs = 30
+batch_size = 30
+target_accuracy = 0.99
+learning_rate = 0.003
+accuracy_threshold = 0.5
+## Hyper parameter
+
+
+model = NFResNet18(class_num=4).to(device)
+print('==== model info ====')
+summary(model, (3, 224, 224))
+print('====================')
+
+
+macs, params = get_model_complexity_info(model,
+                                         (3, 224, 224),
+                                         as_strings=True,
+                                         print_per_layer_stat=True, verbose=True)
+print('{:<30}  {:<8}'.format('Computational complexity: ', macs))
+print('{:<30}  {:<8}'.format('Number of parameters: ', params))
+
+
+
+
+
+## no Train Model Save
+
+model.eval()
+
+## no Train Model Save
+
+
+datasets = FIATClassificationDataset('C://Github//DeepLearningStudy//dataset//FIAT_dataset_food//',
+                                     label_height=224,
+                                     label_width=224,
+                                     isColor=True,
+                                     isNorm=False)
+data_loader = DataLoader(datasets, batch_size=batch_size, shuffle=True)
+
+
+model.train()
+criterion = nn.BCELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+optimizer_agc = AGC(model.parameters(), optimizer)
+
+for epoch in range(training_epochs): # 앞서 training_epochs의 값은 15로 지정함.
+    avg_cost = 0
+    avg_acc = 0
+    total_batch = len(data_loader)
+
+    for X, Y in data_loader:
+        gpu_X = X.to(device)
+        gpu_Y = Y.to(device)
+
+        model.train()
+        optimizer_agc.zero_grad()
+        hypothesis = model(gpu_X)
+        cost = criterion(hypothesis, gpu_Y)
+        cost.backward()
+        avg_cost += (cost / total_batch)
+        optimizer_agc.step()
+
+        model.eval()
+        prediction = model(gpu_X)
+        correct_prediction = torch.argmax(prediction, 1) == torch.argmax(gpu_Y, 1)
+        accuracy = correct_prediction.float().mean()
+        avg_acc += (accuracy / total_batch)
+
+    print('Epoch:', '%04d' % (epoch + 1), 'cost =', '{:.9f}'.format(avg_cost), 'acc =', '{:.9f}'.format(avg_acc))
+    if avg_acc > target_accuracy:
+        break;
+
+## no Train Model Save
+model.eval()
+compiled_model = torch.jit.script(model)
+torch.jit.save(compiled_model, "C://Github//DeepLearningStudy//trained_model//TRAIN_FIAT(NFResNet18).pt")
+## no Train Model Save
+
+print('Learning finished')
